8963ecaa39b2eec4c6d90caa3f507e572689db32
Ram Rustagi <rprustagi@smartron.com>
2019-01-09 16:32:09 +0530

    Lectures for module 1

---
+++ TestPrograms/BadFloatCompute.java
@@ -0,0 +1,5 @@
+public class BadFloatCompute {
+   public static void main(String[] args) {
+      System.out.println((float) 4 / 5);
+   }
+}
---
+++ TestPrograms/BadIntCompute.java
@@ -0,0 +1,6 @@
+public class BadIntCompute {
+   public static void main(String[] args) {
+      //System.out.println(10 * (int) "100" );
+      System.out.println( 4.0 ^ 3);
+   }
+}
